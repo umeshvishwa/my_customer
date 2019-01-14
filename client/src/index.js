b144ccf64e073263b5fa1847ab221fa49b1b556c
Kumar Vishwakarma, U <u.kumar.vishwakarma@accenture.com>
2019-01-15 00:33:30 +0530

    added Fontawesome files

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.scss';
-import 'font-awesome/css/font-awesome.min.css';
+//import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
